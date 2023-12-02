bfd7859e189755a64ddd963635b846552deda006
João Figueiredo <figueiredo1194@gmail.com>
2023-12-02 00:20:46 +0000

    second challenge done

--- Start/Ch_3/challenge_start.py
+++ Start/Ch_3/challenge_start.py
@@ -7,9 +7,17 @@ import pprint
 
 test_str = "2 apples, 9 oranges?, 4 pears, Mike's 1 egg, Jane's 2 kiwis, $50!"
 
-# YOUR CODE HERE
+length = len(test_str)
+digits = len([s for s in test_str if s.isdigit()])
+punctuation = len([s for s in test_str if s in string.punctuation])
+unique_letters = "".join({s for s in test_str if s in string.ascii_letters})
 
 # print the data
 str_data = {
+    "Length": length,
+    "Digits": digits,
+    "Punctuation": punctuation,
+    "Unique Letters": unique_letters,
+    "Unique Count": len(unique_letters),
 }
 pprint.pp(str_data)
